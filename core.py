727594b99e55ef2c04b8dce886c627284ea268ea
Stefanos Patmanidis <stefanpatman91@gmail.com>
2020-10-28 12:00:32 +0200

    Move params to their own file

--- core.py
+++ core.py
@@ -19,7 +19,7 @@ from scipy import optimize
 from math import log
 
 import extensions
-
+import params
 
 ##############################################################################
 ### Helper function
@@ -47,36 +47,6 @@ class Analysis:
     """
     #? Consider locking attributes with __slots__ or @dataclass
 
-    class Param:
-
-        def __init__(self):
-
-            # Method and Algorithm to use
-            self.algorithm = 'nprs'
-            self.method = 'powell'
-
-            # General options
-            self.general = {
-                'persite': False, # These affect effective branch lengths
-                'nsites': 1, # These affect effective branch lengths
-                'round': False, # These affect effective branch lengths
-                'perturb_factor': 0.01,
-                'scalar': False, #! not used right now, should force root age at 1.0
-                'number_of_guesses': 10, # How many times to solve the problem
-                'largeval': 1e30, # For clamping
-                }
-            # Define the behaviour of manual barrier penalty
-            self.barrier = {
-                'manual': True,
-                'max_iterations': 10,
-                'initial_factor':0.25,
-                'multiplier': 0.10,
-                'tolerance': 0.0001,
-                }
-            self.nprs = {
-                'logarithmic': False,
-                'exponent': 2,
-                }
 
     class Array:
         pass
@@ -85,7 +55,7 @@ class Analysis:
 
         random.seed()
 
-        self.param = self.Param()
+        self.param = params.Param()
         self.array = None
         self.tree = None
 
---
+++ params.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+## pyr8s: Divergence Time Estimation
+##
+## Copyright 2020 Stefanos Patmanidis.
+## All rights reserved.
+##############################################################################
+
+"""
+Parameter defaults for use by core.Analysis class.
+To be extended with helpstrings and ranges.
+"""
+
+class Param:
+
+    def __init__(self):
+
+        # Method and Algorithm to use
+        self.algorithm = 'nprs'
+        self.method = 'powell'
+
+        # General options
+        self.general = {
+            'persite': False, # These affect effective branch lengths
+            'nsites': 1, # These affect effective branch lengths
+            'round': False, # These affect effective branch lengths
+            'perturb_factor': 0.01,
+            'scalar': False, #! not used right now, should force root age at 1.0
+            'number_of_guesses': 10, # How many times to solve the problem
+            'largeval': 1e30, # For clamping
+            }
+        # Define the behaviour of manual barrier penalty
+        self.barrier = {
+            'manual': True,
+            'max_iterations': 10,
+            'initial_factor':0.25,
+            'multiplier': 0.10,
+            'tolerance': 0.0001,
+            }
+        self.nprs = {
+            'logarithmic': False,
+            'exponent': 2,
+            }

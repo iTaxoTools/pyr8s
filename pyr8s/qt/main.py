f9f0ea68e34c35b3f692a4cd5bf561c0ef496850
Stefanos Patmanidis <stefanpatman91@gmail.com>
2020-12-13 01:02:46 +0200

    Split qt/main.py into qt/widgets.py

--- pyr8s/qt/main.py
+++ pyr8s/qt/main.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import PyQt5.QtCore as QtCore
 from PyQt5.QtCore import (Qt, QObject, QFileInfo, QState, QStateMachine, QRect, QPoint,
         QSize, QRunnable, QThread, QThreadPool, pyqtSignal, pyqtSlot, QEvent)
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QTabBar,
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QTabBar, QAbstractItemView,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
@@ -20,226 +21,9 @@ from ..param import qt as pqt
 
 from multiprocessing import Process, Pipe
 
-from .utility import UProcess, SyncedWidget, UToolBar
+from .utility import UProcess
+from . import widgets
 from . import icons
-
-class QTreeWidgetCustom(QTreeWidget):
-    """
-    Styled to draw the branches of phylogenetic trees.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.solidColor = QColor('#555555')
-        self.radiusLeaf = 3
-        self.radiusInternal = 5
-        self.setIndentation(16)
-
-    def drawBranches(self, painter, rect, index):
-        super().drawBranches(painter, rect, index)
-
-        solidPen = QPen(self.solidColor)
-        solidPen.setWidth(2)
-        painter.setBrush(self.solidColor)
-        painter.setPen(solidPen)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        indent = self.indentation()
-        item = self.itemFromIndex(index)
-        parent = item.parent()
-        next = None
-        if parent is not None:
-            i = parent.indexOfChild(item)
-            next = parent.child(i+1)
-        child = item.child(0)
-
-        isExpanded = item.isExpanded()
-        hasChildren = child is not None
-        hasMoreSiblings = next is not None
-
-        # Node view for self first
-        segment = QRect(rect)
-        segment.setLeft(rect.width() - indent)
-        rect.setWidth(indent)
-        if not hasChildren:
-            radius = self.radiusLeaf
-            center = segment.center()
-            painter.drawEllipse(center, radius, radius)
-            left = QPoint(center.x()-segment.width()/2, center.y())
-            leftMid = QPoint(center.x()-radius, center.y())
-            painter.drawLine(left, leftMid)
-        else:
-            radius = self.radiusInternal
-            center = segment.center()
-            painter.setBrush(Qt.NoBrush)
-            painter.drawEllipse(center, radius, radius)
-            if isExpanded:
-                bottom = QPoint(center.x(), center.y()+segment.height()/2)
-                bottomMid = QPoint(center.x(), center.y()+radius)
-                painter.drawLine(bottom, bottomMid)
-            else:
-                painter.setBrush(self.solidColor)
-                painter.drawEllipse(center, 1, 1)
-            if parent is None:
-                return
-            left = QPoint(center.x()-segment.width()/2, center.y())
-            leftMid = QPoint(center.x()-radius, center.y())
-            painter.drawLine(left, leftMid)
-
-        # Branch view from direct parent
-        segment.moveLeft(segment.left() - indent)
-        if segment.left() < 0:
-            return
-        if hasMoreSiblings:
-            center = segment.center()
-            right = QPoint(center.x()+segment.width()/2, center.y())
-            painter.drawLine(center, right)
-            top = QPoint(center.x(), segment.top())
-            bottom = QPoint(center.x(), segment.bottom())
-            painter.drawLine(top, bottom)
-        else:
-            center = segment.center()
-            right = QPoint(center.x()+segment.width()/2, center.y())
-            painter.drawLine(center, right)
-            top = QPoint(center.x(), segment.top())
-            painter.drawLine(top, center)
-
-        # Branch extensions for ancestors
-        segment.moveLeft(segment.left() - indent)
-        item = parent
-        while parent is not None:
-            parent = item.parent()
-            next = None
-            if parent is not None:
-                i = parent.indexOfChild(item)
-                next = parent.child(i+1)
-            hasMoreSiblings = next is not None
-            if hasMoreSiblings:
-                center = segment.center()
-                top = QPoint(center.x(), segment.top())
-                bottom = QPoint(center.x(), segment.bottom())
-                painter.drawLine(top, bottom)
-            segment.moveLeft(segment.left() - indent)
-            item = parent
-
-
-        color = self.palette().color(QPalette.Shadow)
-        # if isExpanded:
-        #     color = self.palette().color(QPalette.Mid)
-
-        brush = QBrush()
-        brush.setColor(color)
-        brush.setStyle(Qt.SolidPattern)
-
-        # rect.setLeft(rect.width() - 2*indent)
-        # rect.setWidth(indent)
-        # painter.fillRect(rect, brush)
-
-        # opt = self.viewOptions()
-        # opt.rect = rect
-        # opt.state = QStyle.State_Item | QStyle.State_Sibling
-        # self.style().drawPrimitive(QStyle.PE_IndicatorBranch, opt, painter, self)
-
-class TreeWidgetNode(QTreeWidgetItem):
-    """Linked to a dendropy tree"""
-    def __init__(self, parent, node):
-        """
-        Creates a widget from a dendropy node and adds it to the parent.
-        Recursively creates children widgets from children nodes.
-        Updates node when values are changed.
-        """
-        super().__init__(parent)
-        self.node = node
-        label = str(node.label)
-        label = '[' + label + ']' if node.is_name_dummy else label
-        min = '-' if node.min is None else str(node.min)
-        max = '-' if node.max is None else str(node.max)
-        fix = '-' if node.fix is None else str(node.fix)
-        self.setText(0, label)
-        self.setText(1, min)
-        self.setText(2, max)
-        self.setText(3, fix)
-        self.setTextAlignment(1, Qt.AlignCenter)
-        self.setTextAlignment(2, Qt.AlignCenter)
-        self.setTextAlignment(3, Qt.AlignCenter)
-        self.setFlags(Qt.ItemIsSelectable |
-                      Qt.ItemIsEnabled |
-                      Qt.ItemIsEditable)
-        # self.setIcon(0, QIcon(":/icons/search.png"))
-        for child in node.child_node_iter():
-            # print(child)
-            TreeWidgetNode(self, child)
-        self.setExpanded(True)
-
-    def editFloat(self, value):
-        if value == '' or value == '-':
-            return (None, '-')
-        value = float(value)
-        label = str(int(value)) if value.is_integer() else str(value)
-        return (value, label)
-
-    def showWarning(self, message):
-        QMessageBox.warning(None, 'Warning',
-            message, QMessageBox.Ok)
-
-    def showException(self, message):
-        print(message)
-        QMessageBox.critical(None, 'Error',
-            message, QMessageBox.Ok)
-
-    def checkConstraints(self):
-        if self.node.fix is not None:
-            if self.node.min is not None or self.node.max is not None:
-                self.showWarning('Min/Max constraints are ignored if node is Fixed.')
-        if (self.node.min is not None and
-            self.node.max is not None and
-            self.node.min > self.node.max):
-            self.showWarning('Impossible constraints: Min > Max.')
-
-    def setNodeLabel(self, value):
-        indexString = '[' + str(self.node.index) + ']'
-        if value == '' or value == indexString:
-            self.node.label = str(self.node.index)
-            self.node.is_name_dummy = True
-            return indexString
-        if re.match('^[^,;()\[\]\r\n]*$', value) is None:
-            raise ValueError('Label contains invalid characters.')
-        self.node.is_name_dummy = False
-        self.node.label = value
-        return value
-
-    def setNodeMin(self, value):
-        (self.node.min, label) = self.editFloat(value)
-        self.checkConstraints()
-        return label
-
-    def setNodeMax(self, value):
-        (self.node.max, label) = self.editFloat(value)
-        self.checkConstraints()
-        return label
-
-    def setNodeFix(self, value):
-        (self.node.fix, label) = self.editFloat(value)
-        self.checkConstraints()
-        return label
-
-    def setData(self, column, role, value):
-        # print('DATA',self, column, role, value)
-        # if role == Qt.ItemDataRole.DisplayRole or \
-        try:
-            if role == Qt.ItemDataRole.EditRole:
-                if column == 0:
-                    value = self.setNodeLabel(value)
-                elif column == 1:
-                    value = self.setNodeMin(value)
-                elif column == 2:
-                    value = self.setNodeMax(value)
-                elif column == 3:
-                    value = self.setNodeFix(value)
-        except Exception as exception:
-            self.showException('Error: ' + str(exception))
-        else:
-            super().setData(column, role, value)
 
 
 class Main(QDialog):
@@ -265,6 +49,7 @@ class Main(QDialog):
         (self.analysis,) = state
 
     def fail(self, exception):
+        raise exception
         print(str(exception))
         QMessageBox.critical(None, 'Exception occured',
             str(exception), QMessageBox.Ok)
@@ -282,7 +67,7 @@ class Main(QDialog):
         idle.assignProperty(self.findWidget, 'enabled', True)
         idle.addTransition(self.signalRun, running)
         def onIdleEntry(event):
-            self.runButton.setFocus(True)
+            self.constraintsWidget.setFocus()
         idle.onEntry = onIdleEntry
 
         running.assignProperty(self.runButton, 'visible', False)
@@ -293,7 +78,7 @@ class Main(QDialog):
         running.addTransition(self.signalIdle, idle)
         def onRunningEntry(event):
             self.cancelButton.setFocus(True)
-        idle.onEntry = onRunningEntry
+        running.onEntry = onRunningEntry
 
         self.machine.addState(idle)
         self.machine.addState(running)
@@ -303,8 +88,10 @@ class Main(QDialog):
 
     def eventFilter(self, source, event):
         if event.type() == QEvent.KeyPress:
-            if source == self.constraintsWidget and \
-                event.key() == Qt.Key_Return:
+            if (source == self.constraintsWidget and
+                event.key() == QtCore.Qt.Key_Return and
+                self.constraintsWidget.state() !=
+                    QAbstractItemView.EditingState):
                 self.actionRun()
                 return True
         return QObject.eventFilter(self, source, event)
@@ -315,7 +102,7 @@ class Main(QDialog):
         self.rightPane, self.barButton  = self.createPaneCanvas()
         self.barButton.sync(self.barLabel)
 
-        splitter = QSplitter(Qt.Horizontal)
+        splitter = QSplitter(QtCore.Qt.Horizontal)
         splitter.addWidget(self.leftPane)
         splitter.addWidget(self.rightPane)
         splitter.setStretchFactor(0,0)
@@ -340,12 +127,12 @@ class Main(QDialog):
         actionOpen.setStatusTip('Open an existing file')
         actionOpen.triggered.connect(self.actionOpen)
 
-        toolbar = UToolBar('Tools')
+        toolbar = widgets.UToolBar('Tools')
         toolbar.addAction(actionOpen)
         toolbar.addAction('Save', lambda: self.barButton.setMinimumHeight(68))
         # toolbar.addAction('Export', find)
         toolbar.addAction(exitAct)
-        toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        toolbar.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
         #toolbar.setStyleSheet("background-color:red;")
 
         canvas = QGroupBox()
@@ -369,7 +156,7 @@ class Main(QDialog):
         # def onItemChanged(item, column):
         #     print('CHANGE', self.analysis.param.general.scalar )
 
-        self.constraintsWidget = QTreeWidgetCustom()
+        self.constraintsWidget = widgets.TreeWidgetPhylogenetic()
         self.constraintsWidget.itemActivated.connect(onDoubleClick)
         # self.constraintsWidget.itemChanged.connect(onItemChanged)
         self.constraintsWidget.setColumnCount(4)
@@ -383,10 +170,10 @@ class Main(QDialog):
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
         headerItem = self.constraintsWidget.headerItem()
-        headerItem.setTextAlignment(0, Qt.AlignLeft)
-        headerItem.setTextAlignment(1, Qt.AlignCenter)
-        headerItem.setTextAlignment(2, Qt.AlignCenter)
-        headerItem.setTextAlignment(3, Qt.AlignCenter)
+        headerItem.setTextAlignment(0, QtCore.Qt.AlignLeft)
+        headerItem.setTextAlignment(1, QtCore.Qt.AlignCenter)
+        headerItem.setTextAlignment(2, QtCore.Qt.AlignCenter)
+        headerItem.setTextAlignment(3, QtCore.Qt.AlignCenter)
         self.constraintsWidget.setUniformRowHeights(True)
         self.constraintsWidget.setStyleSheet(
             """
@@ -412,7 +199,7 @@ class Main(QDialog):
             """
             )
         self.constraintsWidget.installEventFilter(self)
-        
+
         layout = QHBoxLayout()
         layout.setContentsMargins(5, 5, 5, 5)
         layout.addWidget(self.constraintsWidget)
@@ -436,7 +223,7 @@ class Main(QDialog):
         pane = QWidget()
 
         label = QLabel("Time & Rate Divergence Analysis")
-        label.setAlignment(Qt.AlignCenter)
+        label.setAlignment(QtCore.Qt.AlignCenter)
         #label.setStyleSheet("background-color:green;")
         labelLayout = QHBoxLayout()
         labelLayout.addWidget(label, 1)
@@ -445,7 +232,7 @@ class Main(QDialog):
         labelWidget.setLayout(labelLayout)
         self.labelTree = label
 
-        toolbar = UToolBar('Tools')
+        toolbar = widgets.UToolBar('Tools')
         toolbar.addWidget(labelWidget)
         #toolbar.setStyleSheet("background-color:red;")
 
@@ -468,13 +255,13 @@ class Main(QDialog):
                 self.constraintsWidget.clearSelection()
                 return
             self.constraintsWidget.clearSelection()
-            found = self.constraintsWidget.findItems(what, Qt.MatchContains | Qt.MatchRecursive)
+            found = self.constraintsWidget.findItems(what, QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive)
             for item in found:
                 item.setSelected(True)
                 self.constraintsWidget.scrollToItem(item)
 
         pixmap = QPixmap(':/icons/search.png')
-        mask = pixmap.createMaskFromColor(QColor('black'), Qt.MaskOutColor)
+        mask = pixmap.createMaskFromColor(QColor('black'), QtCore.Qt.MaskOutColor)
         palette = QGuiApplication.palette()
         pixmap.fill(palette.color(QPalette.Shadow))
         pixmap.setMask(mask)
@@ -611,7 +398,7 @@ class Main(QDialog):
             self.labelTree.setText(labelText)
             self.constraintsWidget.clear()
             # self.analysis.tree.print_plot(show_internal_node_labels=True)
-            TreeWidgetNode(self.constraintsWidget, self.analysis.tree.seed_node)
+            widgets.TreeWidgetNodeConstraints(self.constraintsWidget, self.analysis.tree.seed_node)
 
             header = self.constraintsWidget.header()
             header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
@@ -628,7 +415,7 @@ def show(sys):
     """Entry point"""
     app = QApplication(sys.argv)
     main = Main()
-    main.setWindowFlags(Qt.Window)
+    main.setWindowFlags(QtCore.Qt.Window)
     main.show()
     if len(sys.argv) >= 2:
         main.actionOpenFile(sys.argv[1])
---
+++ pyr8s/qt/widgets.py
@@ -0,0 +1,230 @@
+"""Custom widgets for PyQt5"""
+
+import PyQt5.QtCore as QtCore
+import PyQt5.QtWidgets as QtWidgets
+import PyQt5.QtGui  as QtGui
+
+import re
+
+##############################################################################
+### Phylogenetic Trees
+
+class TreeWidgetPhylogenetic(QtWidgets.QTreeWidget):
+    """Styled to draw the branches of phylogenetic trees."""
+    def __init__(self):
+        super().__init__()
+        self.solidColor = QtGui.QColor('#555555')
+        self.radiusLeaf = 3
+        self.radiusInternal = 5
+        self.setIndentation(16)
+
+    def drawBranches(self, painter, rect, index):
+        """Manually paint branches and nodes"""
+        super().drawBranches(painter, rect, index)
+
+        solidPen = QtGui.QPen(self.solidColor)
+        solidPen.setWidth(2)
+        painter.setBrush(self.solidColor)
+        painter.setPen(solidPen)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+
+        indent = self.indentation()
+        item = self.itemFromIndex(index)
+        parent = item.parent()
+        next = None
+        if parent is not None:
+            i = parent.indexOfChild(item)
+            next = parent.child(i+1)
+        child = item.child(0)
+
+        isExpanded = item.isExpanded()
+        hasChildren = child is not None
+        hasMoreSiblings = next is not None
+
+        # Paint own Node first
+        segment = QtCore.QRect(rect)
+        segment.setLeft(rect.width() - indent)
+        rect.setWidth(indent)
+        if not hasChildren:
+            radius = self.radiusLeaf
+            center = segment.center()
+            painter.drawEllipse(center, radius, radius)
+            left = QtCore.QPoint(center.x()-segment.width()/2, center.y())
+            leftMid = QtCore.QPoint(center.x()-radius, center.y())
+            painter.drawLine(left, leftMid)
+        else:
+            radius = self.radiusInternal
+            center = segment.center()
+            painter.setBrush(QtCore.Qt.NoBrush)
+            painter.drawEllipse(center, radius, radius)
+            if isExpanded:
+                bottom = QtCore.QPoint(center.x(), center.y()+segment.height()/2)
+                bottomMid = QtCore.QPoint(center.x(), center.y()+radius)
+                painter.drawLine(bottom, bottomMid)
+            else:
+                painter.setBrush(self.solidColor)
+                painter.drawEllipse(center, 1, 1)
+            if parent is None:
+                return
+            left = QtCore.QPoint(center.x()-segment.width()/2, center.y())
+            leftMid = QtCore.QPoint(center.x()-radius, center.y())
+            painter.drawLine(left, leftMid)
+
+        # Branch towards direct parent
+        segment.moveLeft(segment.left() - indent)
+        if segment.left() < 0:
+            return
+        if hasMoreSiblings:
+            center = segment.center()
+            right = QtCore.QPoint(center.x()+segment.width()/2, center.y())
+            painter.drawLine(center, right)
+            top = QtCore.QPoint(center.x(), segment.top())
+            bottom = QtCore.QPoint(center.x(), segment.bottom())
+            painter.drawLine(top, bottom)
+        else:
+            center = segment.center()
+            right = QtCore.QPoint(center.x()+segment.width()/2, center.y())
+            painter.drawLine(center, right)
+            top = QtCore.QPoint(center.x(), segment.top())
+            painter.drawLine(top, center)
+
+        # Branch extensions for ancestors
+        segment.moveLeft(segment.left() - indent)
+        item = parent
+        while parent is not None:
+            parent = item.parent()
+            next = None
+            if parent is not None:
+                i = parent.indexOfChild(item)
+                next = parent.child(i+1)
+            hasMoreSiblings = next is not None
+            if hasMoreSiblings:
+                center = segment.center()
+                top = QtCore.QPoint(center.x(), segment.top())
+                bottom = QtCore.QPoint(center.x(), segment.bottom())
+                painter.drawLine(top, bottom)
+            segment.moveLeft(segment.left() - indent)
+            item = parent
+
+
+class TreeWidgetNodeConstraints(QtWidgets.QTreeWidgetItem):
+    """Constraints on an extended dendropy tree"""
+    def __init__(self, parent, node):
+        """
+        Creates a widget from a dendropy node and adds it to the parent.
+        Recursively creates children widgets from children nodes.
+        Updates node when values are changed.
+        """
+        super().__init__(parent)
+        self.node = node
+        label = str(node.label)
+        label = '[' + label + ']' if node.is_name_dummy else label
+        min = '-' if node.min is None else str(node.min)
+        max = '-' if node.max is None else str(node.max)
+        fix = '-' if node.fix is None else str(node.fix)
+        self.setText(0, label)
+        self.setText(1, min)
+        self.setText(2, max)
+        self.setText(3, fix)
+        self.setTextAlignment(1, QtCore.Qt.AlignCenter)
+        self.setTextAlignment(2, QtCore.Qt.AlignCenter)
+        self.setTextAlignment(3, QtCore.Qt.AlignCenter)
+        self.setFlags(QtCore.Qt.ItemIsSelectable |
+                      QtCore.Qt.ItemIsEnabled |
+                      QtCore.Qt.ItemIsEditable)
+        for child in node.child_node_iter():
+            TreeWidgetNodeConstraints(self, child)
+        self.setExpanded(True)
+
+    def showWarning(self, message):
+        QtWidgets.QMessageBox.warning(None, 'Warning',
+            message, QWidgets.QtWidgets.Ok)
+
+    def showException(self, message):
+        print(message)
+        QtWidgets.QMessageBox.critical(None, 'Error',
+            message, QtWidgets.QMessageBox.Ok)
+
+    def checkConstraints(self):
+        if self.node.fix is not None:
+            if self.node.min is not None or self.node.max is not None:
+                self.showWarning('Min/Max constraints are ignored if node is Fixed.')
+        if (self.node.min is not None and
+            self.node.max is not None and
+            self.node.min > self.node.max):
+            self.showWarning('Impossible constraints: Min > Max.')
+
+    def editFloat(self, value):
+        if value == '' or value == '-':
+            return (None, '-')
+        value = float(value)
+        label = str(int(value)) if value.is_integer() else str(value)
+        return (value, label)
+
+    def setNodeMin(self, value):
+        (self.node.min, label) = self.editFloat(value)
+        self.checkConstraints()
+        return label
+
+    def setNodeMax(self, value):
+        (self.node.max, label) = self.editFloat(value)
+        self.checkConstraints()
+        return label
+
+    def setNodeFix(self, value):
+        (self.node.fix, label) = self.editFloat(value)
+        self.checkConstraints()
+        return label
+
+    def setNodeLabel(self, value):
+        indexString = '[' + str(self.node.index) + ']'
+        if value == '' or value == indexString:
+            self.node.label = str(self.node.index)
+            self.node.is_name_dummy = True
+            return indexString
+        if re.match('^[^,;()\[\]\r\n]*$', value) is None:
+            raise ValueError('Label contains invalid characters.')
+        self.node.is_name_dummy = False
+        self.node.label = value
+        return value
+
+    def setData(self, column, role, value):
+        try:
+            if role == QtCore.Qt.ItemDataRole.EditRole:
+                if column == 0:
+                    value = self.setNodeLabel(value)
+                elif column == 1:
+                    value = self.setNodeMin(value)
+                elif column == 2:
+                    value = self.setNodeMax(value)
+                elif column == 3:
+                    value = self.setNodeFix(value)
+        except Exception as exception:
+            self.showException('Error: ' + str(exception))
+        else:
+            super().setData(column, role, value)
+
+
+##############################################################################
+### Layout
+
+class SyncedWidget(QtWidgets.QWidget):
+    """Sync height with other widgets"""
+    syncSignal = QtCore.pyqtSignal()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        if event.size().height() != event.oldSize().height():
+            self.syncSignal.emit()
+
+    def syncHandle(self):
+        other = self.sender().height()
+        self.setMinimumHeight(other)
+
+    def sync(self, widget):
+        self.syncSignal.connect(widget.syncHandle)
+        widget.syncSignal.connect(self.syncHandle)
+
+class UToolBar(QtWidgets.QToolBar, SyncedWidget):
+    syncSignal = QtCore.pyqtSignal()
+    pass
